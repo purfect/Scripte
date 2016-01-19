9f27b68e60884e3a92c0cabb5ff533256150ca5a
purfect <axxit666@gmx.de>
2016-01-19 16:45:01 +0100

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -2,28 +2,35 @@
 #
 # Liest NginX-Logfile ein und gibt sie etwas übersichtlicher aus
 #
-if [ $# -eq 0 ]
+###### CONFIG ############
+FILE=/var/log/nginx/access.log
+LOCALNET='192.168'
+##########################
+
+if [ $# != 0 ]
 then
-	FILE=/var/log/nginx/access.log
-else
-	if [ -f /var/log/nginx/access.log.$1 ]
+	if [ -f $FILE.$1 ]
 	then
-		FILE=/var/log/nginx/access.log.$1
+		FILE=$FILE.$1
 	else
-		echo -e "[!] File not found!"
+		echo -e "[!] File not found!\n"
 		exit 1
 	fi
 fi
-COUNT=$(cat $FILE | wc -l)
-echo -e "\n[*] $COUNT Zugriffe\n"
-if [ $COUNT != 0 ]
+ALLCOUNT=$(cat $FILE | wc -l)
+INTERNCOUNT=$(cat $FILE | grep $LOCALNET | wc -l)
+echo -e "\n[*] $ALLCOUNT Zugriffe insgesamt\n"
+if [ $ALLCOUNT != 0 ]
 then
+	let DIFF=$ALLCOUNT-$INTERNCOUNT
+	echo -e "[*] $INTERNCOUNT LAN-Zugriff(e)\n"
+	echo -e "[*] $DIFF externe Zugriff(e) \n"
+
 	echo -e "\tIP\tAnfrage\tZiel\t\tBrowser"
-	cat $FILE | awk '{FS="\"";print $1,$2,$6}' | awk '{print $1"\t"$6"\t"$7"\t\t"$9}' | grep -v '192.168' | uniq
-	IP=$(cat $FILE | awk '{print $1}' | grep -v '192.168' | uniq) 
+	cat $FILE | awk '{FS="\"";print $1,$2,$6}' | awk '{print $1"\t"$6"\t"$7"\t\t"$9}' | grep -v $LOCALNET | uniq
+	IP=$(cat $FILE | awk '{print $1}' | grep -v $LOCALNET | uniq) 
 	echo -e "\n[*] HOSTS\n"
-	getent hosts $IP | awk '{print $1"\t"$2}' | uniq | grep -v 'local' | grep -v -i 'xer'
+	getent hosts $IP | awk '{print $1"\t"$2}' | uniq 
 fi	
 
 echo -e "\n[*] Beendet mit Exit-Code: $(echo $?)\n"
-
