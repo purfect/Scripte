943b4baaa7be13c526ceb16a7b9556b4bd73e826
Rasputin <axxit666@gmx.de>
2017-11-01 12:24:59 +0100

    Überarbeitung

---
+++ openvpn_auswahl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PFAD="/home/torsten/tmp/vpntest/"
+
+echo "###################################"
+echo -e "\tOPENVPN Standorte"
+echo "###################################"
+for entry in $PFAD*
+do
+	if [[ "$entry" == *.opvpn ]]
+	then
+		echo "$entry" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'
+	fi
+done
+echo "Welcher Standort soll verwendet werden?"
+read LOCATION
+
+SUCHE=$(find "$PFAD" -iname "$LOCATION*") 
+SUCHECOUNT=$(find "$PFAD" -iname "$LOCATION*" | wc -l) 
+if [ "$SUCHECOUNT" == 0 ]
+then
+	echo "Es wurde keine passende Konfiguration gefunden!" 
+	exit 1
+elif [ "$SUCHECOUNT" -ge 2 ]
+then 
+	echo "Es wurden zu viel Standorte mit dem Suchstring gefunden!"
+	exit 1
+else 
+	echo "$SUCHE wurde ausgewählt" 
+	openvpn $SUCHE
+	
+fi
