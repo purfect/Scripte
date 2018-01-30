1eeb120d485e6924f7ee43e55fca659ab8f2e2c1
Rasputin <axxit666@gmx.de>
2018-01-30 04:03:26 +0100

    Überarbeitung

---
+++ ca/install_ca.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CAFILE="https://raw.githubusercontent.com/purfect/Scripte/master/ca/ca.pub"
+USER="rasputin"
+
+echo "[*] Herunterladen der CA"
+wget $CAFILE -O /etc/ssh/ca.pub
+echo "TrustedUserCAKeys /etc/ssh/ca.pub" >> /etc/ssh/sshd_config
+echo "[*] Einrichten der Principals"
+mkdir /etc/ssh/auth_principals
+echo "TrustedUserCAKeys /etc/ssh/ca.pub" >> /etc/ssh/sshd_config
+echo -e 'zone-webservers\nroot-everywhere' > /etc/ssh/auth_principals/root
+echo -e 'zone-webservers\nroot-everywhere' > /etc/ssh/auth_principals/$USER
+echo "[*] Neustarten des SSH-Dienstes"
+/bin/systemctl restart sshd.service
+echo "[*] Einrichtung angeschlossen!"
+
