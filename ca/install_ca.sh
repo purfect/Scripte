e7504369f22df62b58470b577bc33f6ba00953a9
Rasputin <axxit666@gmx.de>
2018-01-30 04:27:26 +0100

    Überarbeitung

--- ca/install_ca.sh
+++ ca/install_ca.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#CAFILE="https://raw.githubusercontent.com/purfect/Scripte/master/ca/ca.pub"
 USER="rasputin"
 
 echo "[*] Anlegen der CA"
-#wget $CAFILE -O /etc/ssh/ca.pub
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC64uz6nTWr52gYyU6OYQzud1nCIBxVA6Gk2PusJRM8TO3dERkP9uErUewz2gQPx3/6bQ8R1NQ/BuOGdHot6CVoDqBaEM8wH2fz6MdhSZEowEXARkZz3Kc/dcogTxqvkmZPGKCQd4zRKr82GEvBNxYoXIzm8R/HY9SOhF/ONOVtGUp4YimSH/4ZGQmXwzd4/HQl2tLkY6zSxXjhK136KPBW1WqmzFUFe4oF+n72DvweEwIdt4qb3zgcbaBmNJ8pg2S+FYfuoGLghkLFIYSKC5ZrKavF77Ogsh/l9lpb4EekcTHwsmSbCyMeBHb62Ps8QWRE9soXuo1h5CDt3VIgafG9 CA" > /etc/ssh/ca.pub
 echo "TrustedUserCAKeys /etc/ssh/ca.pub" >> /etc/ssh/sshd_config
 echo "[*] Einrichten der Principals"
