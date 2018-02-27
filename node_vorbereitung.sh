bd5a3eea33dbe49e74091b54b6f000eff0184887
Rasputin <axxit666@gmx.de>
2018-02-27 16:38:06 +0100

    Überarbeitung

--- node_vorbereitung.sh
+++ node_vorbereitung.sh
@@ -10,10 +10,10 @@ sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ yes/g' /etc/ssh/ss
 echo -e "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sed -i 's/UsePAM\ yes/UsePAM\ no/g' /etc/ssh/sshd_config
 systemctl restart sshd.service
-echo -e "192.168.0.2\t ubuntu01" >> /etch/hosts
-echo -e "192.168.0.3\t ubuntu02" >> /etch/hosts
-echo -e "192.168.0.4\t ubuntu03" >> /etch/hosts
-echo -e "192.168.0.5\t lb" >> /etch/hosts
+echo -e "192.168.0.2\t ubuntu01" >> /etc/hosts
+echo -e "192.168.0.3\t ubuntu02" >> /etc/hosts
+echo -e "192.168.0.4\t ubuntu03" >> /etc/hosts
+echo -e "192.168.0.5\t lb" >> /etc/hosts
 apt-get install software-properties-common -y
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.hosteurope.de/mirror/mariadb.org/repo/10.2/ubuntu xenial main'
