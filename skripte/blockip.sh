2bb9e99bec77fa12e94f5a608baa23bc10a76a07
Administrator <admin@example.com>
2018-08-21 20:45:18 +0200

    Initial commit

--- skripte/blockip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-	iptables -A INPUT -s $1 -j DROP
-	iptables-save | uniq | iptables-restore
-	iptables-save > /etc/iptables/rules.v4
-fi
